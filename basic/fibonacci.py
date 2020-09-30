e4daaf1b86e7ef31ef6363cab2943f69a8d9a8ed
Kushal Borkar <kushalborkar.11@gmail.com>
2020-10-01 02:45:46 +0530

    Update fibonacci.py

--- basic/fibonacci.py
+++ basic/fibonacci.py
@@ -8,22 +8,21 @@ def fibonacci(number:int) -> int:
     >>> fibonacci(number=10)
     55
     
-    """
-    def fibonacci(n): 
-		a = 0
-		b = 1
-		if n < 0: 
-		    return -1 
-		elif n == 0: 
-		    return a 
-		elif n == 1: 
-		    return b 
-		else: 
-		    for _ in range(2,n): 
-		        c = a + b 
-		        a = b 
-		        b = c 
-		    return b
+    """ 
+	a = 0
+	b = 1
+	if n < 0: 
+	    return -1 
+	elif n == 0: 
+	    return a 
+	elif n == 1: 
+	    return b 
+	else: 
+	    for _ in range(2,n): 
+	        c = a + b 
+	        a = b 
+	        b = c 
+	    return b
 
 def fibonacciSequence(number:int) -> List[int]:
     """
