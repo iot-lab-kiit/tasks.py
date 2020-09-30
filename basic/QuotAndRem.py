ce4aa6d7b166585aa9cfaa5e1d3dc0d00de2c104
SamriddhiMishra <43378325+SamriddhiMishra@users.noreply.github.com>
2020-10-01 02:33:24 +0530

    quotient and remainder by dividing two numbers

---
+++ basic/QuotAndRem.py
@@ -0,0 +1,6 @@
+# Find quotient and remainder obtained by dividing two numbers
+a = int(input("Enter first number :"))
+b = int(input("Enter second number :"))
+
+print("Quotient on dividing ",a," by ",b," is :", a//b)
+print("Remainder on dividing ",a," by ",b," is :", a%b)
