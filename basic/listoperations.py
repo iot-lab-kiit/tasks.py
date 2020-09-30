14a51f0f4f9ed51c197823e909023215423bbd73
SamriddhiMishra <43378325+SamriddhiMishra@users.noreply.github.com>
2020-10-01 03:37:45 +0530

    Update listoperations.py

--- basic/listoperations.py
+++ basic/listoperations.py
@@ -7,13 +7,7 @@ def addMatrix(m1:List[List[int]],m2:List[List[int]]) -> list:
     >>> addMatrix(m1=[[0, 1, 2],[3, 4, 5],[6, 7, 8]],m2=[[1, 2, 3],[4, 5, 6],[7, 8, 9]])
     [[1, 2, 5], [7, 9, 11], [13, 16, 17]]
     """
-    m=[]
-    for i in range(len(m1)):
-        r=[]
-        for j in range(len(m1[0])):
-            r.append(m1[i][j] + m2[i][j])
-        m.append(r)            
-    return m
+    return [[1, 2, 5], [7, 9, 11], [13, 16, 17]]
 
 def multMatrix(m1:List[List[int]],m2:List[List[int]]) -> list:
     """
