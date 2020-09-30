32b023570b7ce49917dee26400134bb40bafe829
Lashuk1729 <kushalborkar.11@gmail.com>
2020-10-01 02:43:25 +0530

    Adding nth fibonacci code

--- basic/fibonacci.py
+++ basic/fibonacci.py
@@ -10,20 +10,20 @@ def fibonacci(number:int) -> int:
     
     """
     def fibonacci(n): 
-    a = 0
-    b = 1
-    if n < 0: 
-        return -1 
-    elif n == 0: 
-        return a 
-    elif n == 1: 
-        return b 
-    else: 
-        for _ in range(2,n): 
-            c = a + b 
-            a = b 
-            b = c 
-        return b
+		a = 0
+		b = 1
+		if n < 0: 
+		    return -1 
+		elif n == 0: 
+		    return a 
+		elif n == 1: 
+		    return b 
+		else: 
+		    for _ in range(2,n): 
+		        c = a + b 
+		        a = b 
+		        b = c 
+		    return b
 
 def fibonacciSequence(number:int) -> List[int]:
     """
