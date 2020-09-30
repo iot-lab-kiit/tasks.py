67691cbfc60de20ff26a8af109a1af6593659853
Rohan Kakar <40962760+RKRohk@users.noreply.github.com>
2020-10-01 01:13:04 +0530

    Added More questions and Test Cases (#3)
    * added mathematics functions and tests
    
    * added more questions and tests
    
    * more questions
    
    * Add new module and new questions

---
+++ basic/easy.py
@@ -0,0 +1,104 @@
+from typing import List, Tuple
+
+
+def countDistinct(arr:List[int]) -> int:
+    """
+    >>> countDistinct([1,2,2,3,4])
+    3
+    """
+
+    return 3
+
+def convertToUpperCase(string:str) -> str:
+    """
+    >>> convertToUpperCase("hacktoberfest")
+    'HACKTOBERFEST'
+    """
+    return "HACKTOBERFEST"
+
+def sortListAlphabetical(arr:List[str]) -> List[str]:
+    """
+    >>> sortListAlphabetical(["welcome","to","open","source"])
+    ['open', 'source', 'to', 'welcome']
+    """
+    return ['open', 'source', 'to', 'welcome']
+
+def findStringLength(string:str) -> int:
+    """
+    >>> findStringLength("IoT Lab KiiT")
+    12
+    """
+    return 12
+
+def concatenate(string1:str,string2:str) -> str:
+    """
+    Concatenate two strings without using concat()
+
+    >>> concatenate("hello","world")
+    'helloworld'
+    """
+    return 'helloworld'
+
+def reverse_recursion(string:str) -> str:
+    """
+    Reverse string using recursion
+
+    >>> reverse_recursion("github")
+    'buhtig'
+    """
+    return 'buhtig'
+
+def sort_array(arr:List[int]) -> List[int]:
+    """
+    Sort given array without using sort()
+    >>> sort_array([4,3,2,1])
+    [1, 2, 3, 4]
+    """
+    return [1,2,3,4]
+
+def largest_element(arr:List[int]) -> int:
+    """
+    Find the largest element in the array
+    >>> largest_element([1,2,3,4,5])
+    5
+    """
+
+    return 5
+
+def sum_array(arr:List[int]) -> int:
+    """
+    Find sum of all the array elements
+    >>> sum_array([1,2,3])
+    6
+    """
+
+    return 6
+
+def number_of_elements(string:str) -> int:
+    """
+    Find number of elements in the string
+
+
+    """
+
+def largest_of_three(a:int,b:int,c:int) -> int:
+    """
+    Find largest of 3 given numbers
+    >>> largest_of_three(1,2,3)
+    3
+    """
+    return 3
+
+def number_of_vowels_and_consonants(string:str) -> Tuple[int,int]:
+    """
+    Returns number of vowels and consonants in a given string
+    >>> number_of_vowels_and_consonants("IoT Lab KiiT")
+    (5, 5)
+    """
+    return (5,5)
+
+def swap(a:int,b:int):
+    """
+    Swap two numbers
+    """
+
--- main.py
+++ main.py
@@ -1,2 +1,40 @@
-from basic.oddeven import isEven
+import basic.fibonacci
+import basic.listoperations
+import basic.mathematics
+import basic.oddeven
 
+def main():
+    print(
+    """
+    1. Count distinct elements of an array
+    2. Convert string to upper case
+    3. Sort a set of strings in ascending alphabetical order
+    4. Find length of a string without using len()
+    5. Concatenate two strings without using concatenate function
+    6. Reverse a string using recursion
+    7. Sort an array in ascending order without using sort()
+    8. Find the largest element of the array
+    9. Find the sum of array elements
+    10. Find number of elements in the string
+    11. Find largest of 3 elements
+    12. Count number of vowels and consonants in a string
+    13. Swap two given numbers
+    14. Find nPr i.e permutation for given n and r
+    15. Find nCr i.e combination for given n and r
+    16. Multiply two floating point numbers without using '*' operator
+    17. Find quotient and remainder obtained by dividing two numbers
+    18. Find average of two given numbers
+    19. Find matrix addition of two given matrices
+    20. Find matrix multiplication of two given matrices
+    21. Sort words in given sentence in lexographic (dictionary) order
+    22. Calculate time difference between two given time periods
+    23. Print source code of this file.
+    24. Find if given number is even
+    25. Find if given number is odd
+    26. Find n'th fibonacci number for given n
+    27. Return fibonacci sequence up to the given number
+    """)
+
+
+if __name__ == "__main__":
+    main()
