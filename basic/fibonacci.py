bc13b96eda42a2f44871bc442a12bc5f6b6296f2
Ajeet Singh <46717261+raba-ajeet@users.noreply.github.com>
2020-10-01 13:07:14 +0530

    Update fibonacci.py

--- basic/fibonacci.py
+++ basic/fibonacci.py
@@ -2,22 +2,22 @@ from typing import List
 
 # fibonacci numbers has property that f(n)=f(n-1)+f(n-2)
 def fibonacci(number):
-   if number<0: 
+    if number<0: 
         print("Incorrect input") 
     # First Fibonacci number is 0 
-    elif number==1: 
+    elif number==0: 
         return 0
     # Second Fibonacci number is 1 
-    elif number==2: 
+    elif number==1: 
         return 1
     else: 
-        return Fibonacci(number-1)+Fibonacci(number-2)  
+        return fibonacci(number-1)+fibonacci(number-2)   
 
 def fibonacciSequence(number):
     fibonacci_numbers = [0, 1]
-    for i in range(2,number):
+    for i in range(2,number+1):
         fibonacci_numbers.append(fibonacci_numbers[i-1]+fibonacci_numbers[i-2])
-    return fibonacci_numbers;
+    return fibonacci_numbers
 
 
 if __name__ == "__main__":
