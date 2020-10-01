b88e25ab0666a353229e8ec50c47dc4bc2d1c6f7
Samyak Kala <48255425+Sk70249@users.noreply.github.com>
2020-10-02 00:34:23 +0530

    Update longest_common_sequence.py
    documentation added

--- basic/longest_common_sequence.py
+++ basic/longest_common_sequence.py
@@ -1,19 +1,29 @@
-def lcs(s1, s2):
-    cols = len(s1) + 1
-    rows = len(s2) + 1
+def lcs(s1, s2) -> str:
+  """Compare both the strigs and prints
+     the loggest common sequence between
+     both the strings.
 
-    t = [[0 for i in range(cols)] for i in range(rows)]
+  >>> lcs(“ABCDGH”, “AEDFHR”) 
+      is “ADH" i.e. 3
+  """
+  cols = len(s1) + 1
+  rows = len(s2) + 1
 
-    max_length = 0
+  t = [[0 for i in range(cols)] for i in range(rows)]
 
-    for i in range(1, rows):
-        for j in range(1, cols):
-            if s2[i-1] == s1[j-1]:
-                t[i][j] = 1 + t[i-1][j-1]
-            else:
-                t[i][j] = max(t[i-1][j], t[i][j-1])
+  max_length = 0
 
-            max_length = max(max_length, t[i][j])
+  for i in range(1, rows):
+      for j in range(1, cols):
+          if s2[i-1] == s1[j-1]:
+              t[i][j] = 1 + t[i-1][j-1]
+          else:
+              t[i][j] = max(t[i-1][j], t[i][j-1])
 
-    return max_length
-© 2020 GitHub, Inc.
+          max_length = max(max_length, t[i][j])
+
+  print(max_length)
+  
+if __name__ == "__main__":
+  
+  lcs("AGGTAB", "GXTXAYB")
