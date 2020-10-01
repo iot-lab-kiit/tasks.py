8ab2ec8e6f3acfff65a1c338ec36f536443dbae5
zeeshanok <zeeshanv@outlook.com>
2020-10-01 13:38:40 +0400

    Edited string.py with solution to #23

--- basic/strings.py
+++ basic/strings.py
@@ -1,3 +1,6 @@
+from datetime import datetime
+
+
 def iterative_len(x):
     """Return the len of an iterator without using len
     
@@ -21,3 +24,22 @@ def recursive_len(x):
         return 0
 
     return 1 + recursive_len(x[1:])
+
+
+def time_difference(time1: str, time2: str, fmt: str = '%H:%M:%S') -> str:
+    """
+    Returns the difference between `time1` from `time2` as a sting.
+
+    >>> time_difference('12:00:00', '6:00:00')
+    '6:00:00'
+
+    >>> time_difference('6:00:00', '12:00:00')
+    '6:00:00'
+    """
+
+    d1 = datetime.strptime(time1, fmt)
+    d2 = datetime.strptime(time2, fmt)
+
+    # Subtracts the largest from the smallest datetime object
+    return str(max(d1, d2) - min(d1, d2))
+
