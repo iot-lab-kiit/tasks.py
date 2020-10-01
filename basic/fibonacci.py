be71ac3b83a14cdaae9742f10800103f2f3b7be0
Dhruv Kapur <dhruvkapur4@gmail.com>
2020-10-01 21:15:35 +0530

    Implemented nth Fibonacci Number task with O(log n) time complexity

--- basic/fibonacci.py
+++ basic/fibonacci.py
@@ -1,17 +1,40 @@
 from typing import List
 
 
-def fibonacci(number:int) -> int:
-    """
-    Returns the n'th fibonacci sequence number
-    
-    >>> fibonacci(number=10)
-    55
-    
-    """
+# Simple 2d matrix multiplication. The function return the product
+def matrixMul(a, b):
+    # Initializing Empty Matrix
+    c = [[0, 0], [0, 0]]
+    # 2x2 matrix multiplication. Essentially O(1)
+    for i in range(2):
+        for j in range(2):
+            for k in range(2):
+                c[i][j] = (c[i][j] + (a[i][k] * b[k][j]))
 
-    return 55
+    # Returning the products
+    return c
 
+
+# Essentially applying fast exponentiation on the "Magic Matrix"
+# The [1][0] or [0][1] element of the magic matrix raised to the nth power gives the nth fibonacci number
+def fibonacci(n):
+    # Initializing Magic Matrix
+    a = [[1, 1, ], [1, 0]]
+    # Initializing Identity Matrix
+    res = [[1, 0], [0, 1]]
+
+    # Fast Exponentiation (log(n) complexity)
+    while n:
+        if n & 1:
+            res = matrixMul(res, a)
+        n >>= 1
+        a = matrixMul(a, a)
+
+    # Return the nth fibonacci number. Could also return res[0][1] instead.
+    return res[1][0]
+
+
+# Function to return the Fibonacci Sequence till the nth number
 def fibonacciSequence(number:int) -> List[int]:
     """
     Returns a list of first n fibonacci numbers
@@ -22,4 +45,4 @@ def fibonacciSequence(number:int) -> List[int]:
 
 
 if __name__ == "__main__":
-    fibonacci(number=55)    
+    fibonacci(55)    
