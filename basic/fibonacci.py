3d7a94968bad5a9929c011d5f3f827373c7d2e84
Dhruv Kapur <dhruvkapur4@gmail.com>
2020-10-04 00:54:19 +0530

    Solved Inaccuracy in Fibonacci Solution

--- basic/fibonacci.py
+++ basic/fibonacci.py
@@ -1,16 +1,32 @@
 from typing import List
 
-def fibonacci(number:int) -> int:
-    """
-    Returns the n'th fibonacci sequence number
-    
-    >>> fibonacci(number=10)
-    55
-    
-    """
+def matrixMul(a, b):
+    # Initializing Empty Matrix
+    c = [[0, 0], [0, 0]]
+    # 2x2 matrix multiplication. Essentially O(1)
+    for i in range(2):
+        for j in range(2):
+            for k in range(2):
+                c[i][j] = (c[i][j] + (a[i][k] * b[k][j]))
 
-    PHI = (1 + 5 ** 0.5) / 2
-    return int((PHI**number - (-PHI)**(-number))/(5**0.5))
+    # Returning the products
+    return c
+
+def fibonacci(number:int) -> int:
+    # Initializing Magic Matrix
+    a = [[1, 1, ], [1, 0]]
+    # Initializing Identity Matrix
+    res = [[1, 0], [0, 1]]
+
+    # Fast Exponentiation (log(n) complexity)
+    while number:
+        if number & 1:
+            res = matrixMul(res, a)
+        number >>= 1
+        a = matrixMul(a, a)
+
+    # Return the nth fibonacci number. Could also return res[0][1] instead.
+    return res[1][0]
 
 
 def fibonacciSequence(number:int) -> List[int]:
@@ -20,6 +36,4 @@ def fibonacciSequence(number:int) -> List[int]:
     [0,1,1,2,3,5,8,13,21,34,55]
     """
     return [fibonacci(num) for num in range(number + 1)]
-
-
 
